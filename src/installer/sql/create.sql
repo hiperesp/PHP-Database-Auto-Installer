9f4d9c55724fc7b636907ecace36a5bfa37686cf
= <gabrielramos149@gmail.com>
2019-11-01 12:26:08 -0300

    Example SQL file fixed

--- src/installer/sql/create.sql
+++ src/installer/sql/create.sql
@@ -2,5 +2,5 @@ CREATE TABLE `phpDbAutoInstaller` (
          `id`           INT             NOT NULL
         ,`test1`        VARCHAR(255)    NOT NULL
         ,`test2`        VARCHAR(255)    NOT NULL
-        ,PRIMARY KEY(idCliente)
+        ,PRIMARY KEY(id)
 );
